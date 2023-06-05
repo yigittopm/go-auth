b8b0a542f44a11b881b21a3d7c40517975e0b63f
Mert <yigittopm@hotmail.com>
2023-06-05 12:44:43 +0300

    Clean Pattern - Config created

---
+++ cmd/seed/main.go
@@ -0,0 +1,21 @@
+package seedpackage
+
+func main() {
+	//config.ReadConfig()
+	//
+	//db := datastore.NewDB()
+	//db.LogMode(true)
+	//defer db.Close()
+	//
+	//u := &model.User{
+	//	ID:        1,
+	//	Name:      "Tom",
+	//	Age:       "20",
+	//	CreatedAt: nil,
+	//	UpdatedAt: nil,
+	//	DeletedAt: nil,
+	//}
+	//if err := db.Create(u).Error; err != nil {
+	//	log.Fatalf("failed to seed user data: %v", err)
+	//}
+}
